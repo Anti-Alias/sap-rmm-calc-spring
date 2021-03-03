69258c0f9856ef6cdca40377fcf3a10fa4a58aff
William Cahill <drewlikesramen@gmail.com>
2021-03-03 18:28:26 -0500

    Initial commit

---
+++ src/main/java/com/freddiemac/saprmm/SaprmmApplication.java
@@ -0,0 +1,13 @@
+package com.freddiemac.saprmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaprmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaprmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freddiemac/saprmm/SaprmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freddiemac.saprmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaprmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
