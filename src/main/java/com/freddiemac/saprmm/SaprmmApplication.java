b45282774ba60709bab3b111668504b250ae8288
William Cahill <drewlikesramen@gmail.com>
2021-03-03 19:18:37 -0500

    Added sub data table

--- src/main/java/com/freddiemac/saprmm/SaprmmApplication.java
+++ src/main/java/com/freddiemac/saprmm/SaprmmApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SaprmmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SaprmmApplication.class, args);
 	}
-
 }
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,28 @@
 CREATE TABLE "saprmm" (
-    ID SERIAL NOT NULL
-)
+    id SERIAL PRIMARY KEY NOT NULL,
+    pool_percent DOUBLE PRECISION NOT NULL,
+    fm_participation_percent DOUBLE PRECISION NOT NULL,
+    upb_investor_prior_amount DOUBLE PRECISION NOT NULL,
+    mortgage_upb_prior_amount DOUBLE PRECISION NOT NULL,
+    upb_current_amount DOUBLE PRECISION NOT NULL,
+    pi_payment_amount DOUBLE PRECISION NOT NULL,
+    note_rate DOUBLE PRECISION NOT NULL,
+    pool_term DOUBLE PRECISION NOT NULL,
+    product_code TEXT NOT NULL,
+    maturity_date TIMESTAMPTZ NOT NULL,
+    principal_amortization_code TEXT NOT NULL,
+    ddlpi TEXT NOT NULL,
+    active_inactive_edit_code TEXT NOT NULL,
+    upb_adjustment_amount_current DOUBLE PRECISION NOT NULL,
+    loan_status TEXT NOT NULL
+);
+
+CREATE TABLE "saprmm_sub_data" (
+    saprmm_id INTEGER NOT NULL REFERENCES "saprmm" (id),
+    upb DOUBLE PRECISION NOT NULL,
+    rmm TEXT NOT NULL,
+    loan_status TEXT NOT NULL,
+    upb_current_amount DOUBLE PRECISION NOT NULL,
+    maturity_date TIMESTAMPTZ NOT NULL,
+    pool_term DOUBLE PRECISION NOT NULL
+);
